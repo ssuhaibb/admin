e6ef16e19c652f5aa728155935b0d5aaf55b4e43
Mohammad Suhaib <ssuhaibb@gmail.com>
2019-04-05 17:39:24 +0530

    react-table added

--- src/views/Buyers/Buyers.js
+++ src/views/Buyers/Buyers.js
@@ -1,29 +1,100 @@
 import React, { Component } from "react";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import { Link } from "react-router-dom";
+
 import {
-  Badge,
-  Card,
-  CardBody,
-  CardHeader,
-  Col,
-  Pagination,
-  PaginationItem,
-  PaginationLink,
-  Row,
-  Table
+    Badge,
+    Card,
+    CardBody,
+    CardHeader,
+    Col,
+    Pagination,
+    PaginationItem,
+    PaginationLink,
+    Row,
+    Table
 } from "reactstrap";
 
 class Buyers extends Component {
-  render() {
-    return (
-      <div className="animated fadeIn">
-        <Row>
-          <Col xs="12" lg="12">
-            <Card>
-              <CardHeader>
-                 <big>Users</big>
-              </CardHeader>
-              <CardBody>
-                <Table responsive striped>
+    constructor(props) {
+        super(props);
+        this.state = {
+            userData: [
+                { name: "John", age: "23", city: "Texas", role: "Buyer" },
+                { name: "Max", age: "24", city: "Riverdale", role: "Seller" },
+                { name: "Tim", age: "25", city: "Austin", role: "Architect" },
+                { name: "John", age: "23", city: "Texas", role: "Buyer" },
+                { name: "Max", age: "24", city: "Riverdale", role: "Seller" },
+                { name: "Tim", age: "25", city: "Austin", role: "Architect" },
+                { name: "John", age: "23", city: "Texas", role: "Buyer" },
+                { name: "Max", age: "24", city: "Riverdale", role: "Seller" },
+                { name: "Tim", age: "25", city: "Austin", role: "Architect" },
+                { name: "John", age: "23", city: "Texas", role: "Buyer" },
+                { name: "Max", age: "24", city: "Riverdale", role: "Seller" },
+                { name: "Tim", age: "25", city: "Austin", role: "Architect" },
+                { name: "John", age: "23", city: "Texas", role: "Buyer" },
+                { name: "Max", age: "24", city: "Riverdale", role: "Seller" },
+                { name: "Tim", age: "25", city: "Austin", role: "Architect" },
+                { name: "John", age: "23", city: "Texas", role: "Buyer" },
+                { name: "Max", age: "24", city: "Riverdale", role: "Seller" },
+                { name: "Tim", age: "25", city: "Austin", role: "Architect" },
+                { name: "John", age: "23", city: "Texas", role: "Buyer" },
+                { name: "Max", age: "24", city: "Riverdale", role: "Seller" },
+                { name: "Tim", age: "25", city: "Austin", role: "Architect" },
+                { name: "John", age: "23", city: "Texas", role: "Buyer" },
+                { name: "Max", age: "24", city: "Riverdale", role: "Seller" },
+                { name: "Tim", age: "25", city: "Austin", role: "Architect" },
+                { name: "John", age: "23", city: "Texas", role: "Buyer" },
+                { name: "Max", age: "24", city: "Riverdale", role: "Seller" },
+                { name: "Tim", age: "25", city: "Austin", role: "Architect" },
+                { name: "John", age: "23", city: "Texas", role: "Buyer" },
+                { name: "Max", age: "24", city: "Riverdale", role: "Seller" },
+                { name: "Tim", age: "25", city: "Austin", role: "Architect" }
+            ],
+            columns: [
+                {
+                    Header: "Name",
+                    accessor: "name",
+                    maxWidth: 190,
+                    id: "name",
+                    Cell: ({ row }) => <Link to="/">{row.name}</Link>
+                },
+                { Header: "Age", accessor: "age" },
+                { Header: "City", accessor: "city" },
+                { Header: "Role", accessor: "role" },
+                {
+                    Header: "Action",
+                    accessor: "action",
+                    id: "actions",
+                    Cell: ({ row }) => (
+                        <Badge color="success" style={{ cursor: "pointer", textAlign:'center' }}>
+                            More
+                        </Badge>
+                    )
+                }
+            ]
+        };
+    }
+    render() {
+        const rows = this.state.userData;
+        const columns = this.state.columns;
+        return (
+            <div className="animated fadeIn">
+                <Row>
+                    <Col xs="12" lg="12">
+                        <Card>
+                            <CardHeader>
+                                <big>Users</big>
+                            </CardHeader>
+                            <CardBody>
+                                <ReactTable
+                                    data={rows}
+                                    columns={columns}
+                                    className="-striped -highlight"
+                                    defaultPageSize={10}
+                                />
+                                {/* <Table responsive striped>
                   <thead>
                     <tr>
                       <th>Username</th>
@@ -74,38 +145,46 @@ class Buyers extends Component {
                       </td>
                     </tr>
                   </tbody>
-                </Table>
-                <Pagination>
-                  <PaginationItem disabled>
-                    <PaginationLink previous tag="button">
-                      Prev
-                    </PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem active>
-                    <PaginationLink tag="button">1</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink tag="button">2</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink tag="button">3</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink tag="button">4</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink next tag="button">
-                      Next
-                    </PaginationLink>
-                  </PaginationItem>
-                </Pagination>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+                </Table> */}
+                                {/* <Pagination>
+                                    <PaginationItem disabled>
+                                        <PaginationLink previous tag="button">
+                                            Prev
+                                        </PaginationLink>
+                                    </PaginationItem>
+                                    <PaginationItem active>
+                                        <PaginationLink tag="button">
+                                            1
+                                        </PaginationLink>
+                                    </PaginationItem>
+                                    <PaginationItem>
+                                        <PaginationLink tag="button">
+                                            2
+                                        </PaginationLink>
+                                    </PaginationItem>
+                                    <PaginationItem>
+                                        <PaginationLink tag="button">
+                                            3
+                                        </PaginationLink>
+                                    </PaginationItem>
+                                    <PaginationItem>
+                                        <PaginationLink tag="button">
+                                            4
+                                        </PaginationLink>
+                                    </PaginationItem>
+                                    <PaginationItem>
+                                        <PaginationLink next tag="button">
+                                            Next
+                                        </PaginationLink>
+                                    </PaginationItem>
+                                </Pagination> */}
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
 
 export default Buyers;
